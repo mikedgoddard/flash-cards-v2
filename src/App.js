f34cd305309c245ed82c4c26890abd77af4ed3a2
Michael Goddard <deafninja+github@gmail.com>
2026-02-11 20:26:51 -0500

    Add styles, logic to show and hide question and answer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
+import { useState } from 'react';
 import './styles.css';
-export default function App() {
-	return (
-		<div className='App'>
-			<header>
-				<h1>React Flashcard Project:</h1>
-			</header>
-			<main>
-				<FlashCards />
-			</main>
-		</div>
-	);
-	//   return (
-	//     <div className="App">
-	//       <header className="App-header">
-	//         <img src={logo} className="App-logo" alt="logo" />
-	//         <p>
-	//           Edit <code>src/App.js</code> and save to reload.
-	//         </p>
-	//         <a
-	//           className="App-link"
-	//           href="https://reactjs.org"
-	//           target="_blank"
-	//           rel="noopener noreferrer"
-	//         >
-	//           Learn React
-	//         </a>
-	//       </header>
-	//     </div>
-	//   );
-}
 
 const questions = [
 	{
@@ -64,16 +35,49 @@ const questions = [
 		answer: 'Controlled element',
 	},
 ];
+export default function App() {
+	return (
+		<div className='App'>
+			<header>
+				<h1>React Flashcard Project:</h1>
+			</header>
+			<main>
+				<FlashCards />
+			</main>
+		</div>
+	);
+}
 
 function FlashCards() {
+	// Set state for the selected questions/answers based on the id of the object
+	// * Note: this state is scoped to only this FlashCards component
+	const [selectedId, setSelectedId] = useState(null);
+
+	// Create a function to handle the click of the flashcards.  Passing the question.id via props (id)
+	function handleClick(id) {
+		// logic - So we can toggle the selected or not (show/hide answers)
+		// * We are checking to see if the value of the id prop (passed by the handleClick(question.id) on the onClick) does NOT match explicitly
+		//* with the selectId useState var.  If it does not (aka true) then set selectedId(id), if does match (false) then set selectedId(null)
+		setSelectedId(id !== selectedId ? id : null);
+	}
 	return (
 		<section>
 			{/* Lets grab the questions using .map on the questions array - Going to use <ul> for semantics*/}
 			<ul className='flashcards'>
 				{questions.map((question) => (
-					<li>{question.question}</li>
+					<li
+						key={question.id}
+						// we cannot call a function onClick in React.  So we have to use arrow function to call the handleClick() function
+						onClick={() => handleClick(question.id)}
+						// Here we are logically checking if question.id explicitly matches the selectId.  If does (true) then className='selected', if it does not (false) then className=''
+						className={question.id === selectedId ? 'selected' : ''}
+					>
+						{/* This is same logic used for className... if question.id explicitly matches (true) the selectedId then show the question.answer. If does not match (false) then show the question.question  */}
+						{question.id === selectedId
+							? question.answer
+							: question.question}
+					</li>
 				))}
-				;
 			</ul>
 		</section>
 	);
